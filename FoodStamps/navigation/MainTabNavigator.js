52a76e508de3514d257e64fe307dfd2d4e2ebeee
Omar Ibrahim <oibrahim.2727@gmail.com>
2018-10-20 22:11:12 -0700

    add files to repo

---
+++ FoodStamps/navigation/MainTabNavigator.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import StoreScreen from '../screens/StoresScreen';
+import SearchScreen from '../screens/SearchScreen';
+import ReceiptScreen from '../screens/ReceiptScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-home'
+      }
+    />
+  ),
+};
+
+const StoreStack = createStackNavigator({
+  Store: StoreScreen,
+});
+
+StoreStack.navigationOptions = {
+  tabBarLabel: 'Stores',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-map'
+      }
+    />
+  ),
+};
+
+const SearchStack = createStackNavigator({
+  Search: SearchScreen,
+});
+
+SearchStack.navigationOptions = {
+  tabBarLabel: 'Search',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-search'
+      }
+    />
+  ),
+};
+
+const ReceiptStack = createStackNavigator({
+  Receipt: ReceiptScreen,
+});
+
+ReceiptStack.navigationOptions = {
+  tabBarLabel: 'Add Receipt',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'plus'
+      }
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  StoreStack,
+  SearchStack,
+  ReceiptStack,
+  SettingsStack,
+});
